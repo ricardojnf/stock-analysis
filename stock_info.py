a3ccfaf81831be6959cf1402aad1cbc64c9f7da9
ricardojnf <rjn.ferreira@campus.fct.unl.pt>
2021-02-28 21:38:01 +0000

    Stock history added

--- stock_info.py
+++ stock_info.py
@@ -20,4 +20,27 @@ def get_stock_info(symbol):
     st_info = yf.Ticker(symbol).info
     return [st_info[key] for key in key_info]
 
-print(get_stock_info('AMZN'))
+
+def get_stock_history(symbol, timeRange='max'):
+    '''
+    Returns basic info about the stock requested
+
+    Parameters
+    ----------
+    symbol : str
+        Symbol representing the stock in the stock market
+    
+    period : str
+        Time range of the information.
+        Valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+
+    Returns
+    -------
+    Pandas DataFrame
+        A data frame containing historical information about the price values
+        of the stock
+    '''
+
+    st_hist = yf.Ticker(symbol).history(period=timeRange)
+    return st_hist
+
